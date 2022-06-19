e47104e8d38d4bbd6c898ae8e62a11cdbf66f005
st2180 <62060826+st2180@users.noreply.github.com>
2022-06-19 10:39:46 +0100

    refactor DBManager to CheckoutManager

--- func/tests/unit/test_checkout_manager.py
+++ func/tests/unit/test_checkout_manager.py
@@ -3,20 +3,19 @@ import unittest
 
 from orm import CheckoutManager
 
+
 class TestCheckoutManager(unittest.TestCase):
     def test_get_all(self):
         db = CheckoutManager()
         checkouts = db.get_all()
 
         self.assertEqual(len(checkouts), 3)
-        
 
     def test_get(self):
         db = CheckoutManager()
         checkouts = db.get(2)
 
         self.assertEqual(checkouts.id, 2)
-
 
     def test_update_checkout(self):
         pass
